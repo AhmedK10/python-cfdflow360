85a1c7e798f22d713ddb49a96f8eca970117e1d4
Ahmed Khogali <ahmed.khog@outlook.com>
2023-08-29 13:12:06 +0300

    added the ability to upload surface and volume meshes with no errors

--- README.md
+++
@@ -1,2 +0,0 @@
-# python-cfdflow360
-Performing a Computational Fluid Dynamics (CFD) simulation with the use of the Flow360 Python API from Flexcompute.
--- run_case.py
+++ run_case.py
@@ -1,25 +1,56 @@
 import flow360 as fl
-from flow360.examples import OM6wing
 
-OM6wing.get_files()
-
-volume_mesh = fl.VolumeMesh.from_file(OM6wing.mesh_filename, name="OM6wing-mesh")
-volume_mesh = volume_mesh.submit()
-
-params = fl.Flow360Params(
-    geometry=fl.Geometry(
-        ref_area=1.15315084119231,
-        moment_length=(1.47602, 0.801672958512342, 1.47602),
-        mesh_unit="m",
-    ),
-    freestream=fl.Freestream.from_speed((286, "m/s"), alpha=3.06),
-    time_stepping=fl.TimeStepping(CFL=fl.TimeSteppingCFL.adaptive()),
-    boundaries={
-        "1": fl.NoSlipWall(name="wing"),
-        "2": fl.SlipWall(name="symmetry"),
-        "3": fl.FreestreamBoundary(name="freestream"),
+############################################
+params = fl.SurfaceMeshingParams(
+    max_edge_length=0.16,
+    curvatureResolutionAngle= 15,
+    growthRate= 1.2,
+        edges={
+        "leadingEdge": fl.meshing.Aniso(method="angle", value=5),
+        "trailingEdge": fl.meshing.Aniso(method="height", value=0.001),
+        "root": fl.meshing.Aniso(method="aspectRatio", value=10),
+        "tip": fl.meshing.UseAdjacent(),
+        "fuselageSplit": fl.meshing.ProjectAniso(),
+    },
+    faces={
+        "rightWing": fl.meshing.Face(max_edge_length=0.08),
+        "leftWing": fl.meshing.Face(max_edge_length=0.08),
+        "fuselage": fl.meshing.Face(max_edge_length=0.1),
     },
 )
 
-case = volume_mesh.create_case("OM6wing", params)
-case = case.submit()
+surface_mesh = fl.SurfaceMesh.create(
+    "airplane.csm", params=params, name="airplane-new-surf-mesh"
+)
+surface_mesh = surface_mesh.submit()
+
+#####################################
+
+params = fl.VolumeMeshingParams(
+    refinementFactor= 1,
+    volume=fl.meshing.Volume(first_layer_thickness=1e-5),
+)
+
+volume_mesh = surface_mesh.create_volume_mesh("airplane-new-volume-mesh", params=params)
+volume_mesh = volume_mesh.submit()
+
+####################################
+
+# params = fl.Flow360Params(
+#     geometry=fl.Geometry(
+#         ref_area=1.15315084119231,
+#         moment_length=(1.47602, 0.801672958512342, 1.47602),
+#         mesh_unit="m",
+#     ),
+#     freestream=fl.Freestream.from_speed((286, "m/s"), alpha=3.06),
+#     time_stepping=fl.TimeStepping(max_pseudo_steps=500),
+#     boundaries={
+#         "1": fl.NoSlipWall(name="wing"),
+#         "2": fl.SlipWall(name="symmetry"),
+#         "3": fl.FreestreamBoundary(name="freestream"),
+#     },
+# )
+
+
+
+####################################

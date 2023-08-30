740b03b591e26f91e11671a656c0742d3031c8a1
Ahmed Khogali <ahmed.khog@outlook.com>
2023-08-30 20:15:47 +0300

    completed the script testing and added comments

--- run_case.py
+++ run_case.py
@@ -1,34 +1,30 @@
 import flow360 as fl
 import matplotlib.pyplot as plt
 import time
-#from pylab import *
 
-
-
-def wait_for_simulation(case):
+# a function to make sure a simulation is finished before moving on to the next one
+def wait_for_simulation(case, alpha):
     while not case.is_finished():
-        print(f"Waiting for simulation to finish...")
-        time.sleep(30)
+        print(f"Waiting for simulation with alpha={alpha} to finish...")
+        time.sleep(200)
 
-############################################
+alpha_values = []
+cl_values = []
 
-created_cases = []
-angle_of_attack_values = [10] #list(range(-5, 1, 5))
+# iterating through all aoa values and creating seprate simulations
+angle_of_attack_values = list(range(-5, 26, 5))
 for alpha in angle_of_attack_values:
+    # creating and submitting the surface mesh from the json file
     params_surf = fl.SurfaceMeshingParams("surface.json")
     surface_mesh = fl.SurfaceMesh.create("airplane.csm", params=params_surf, name=f"airplane-surf-mesh-alpha({alpha})")
     surface_mesh = surface_mesh.submit()
 
-    #####################################
-
+    # creating and submitting the volume mesh from the json file
     params_vol = fl.VolumeMeshingParams("volume.json")
     volume_mesh = surface_mesh.create_volume_mesh(f"airplane-volume-mesh-alpha({alpha})", params=params_vol)
     volume_mesh = volume_mesh.submit()
 
-    ####################################
-
-
-
+    # creating simulations setup with adjusted aoa values per simulation
     params = fl.Flow360Params(
         geometry=fl.Geometry(
             ref_area=12.5,
@@ -53,14 +49,12 @@ for alpha in angle_of_attack_values:
         navier_stokes_solver=fl.NavierStokesSolver(
             absolute_tolerance=1e-8,
             linear_iterations=35,
-            # kappa_MUSCL=-1,
             order_of_accuracy=2
         ),
         turbulence_model_solver=fl.TurbulenceModelSolver(
             model_type="SpalartAllmaras",
             absolute_tolerance=1e-7,
             linear_iterations=25,
-            # kappa_MUSCL=-1,
             order_of_accuracy=2
         ),
     )
@@ -68,37 +62,23 @@ for alpha in angle_of_attack_values:
     # Create a simulation case for each alpha
     case = volume_mesh.create_case(f"airplane-case-alpha({alpha})", params)
     case = case.submit()
-    #wait_for_simulation(case)
-    created_cases.append(case)
-
-
-# wait_for_simulations(my_cases)
-print("All simulations have been performed")
-
-
-alpha_values = []
-cl_values = []
-
-for case in created_cases:
-    alpha = case.params.freestream.alpha
-    cl = case.results.total_forces.raw["CL"][-1]  # Get the last CL value
-    alpha_values.append(alpha)
+    wait_for_simulation(case, alpha)
+    time.sleep(100)
+    aoa = case.params.freestream.alpha
+    # Getting the last CL value
+    cl = case.results.total_forces.raw["CL"][-1]
+    alpha_values.append(aoa)
     cl_values.append(cl)
 
+# confirmation of finishing all simulations
+print("All simulations have been performed")
 
-
+# making a plot of CL values against corresponding aoa
 plt.plot(alpha_values, cl_values, marker='o')
 plt.xlabel("Angle of Attack (alpha)")
 plt.ylabel("Lift Coefficient (CL)")
 plt.title("CL vs. Alpha")
 plt.grid()
 plt.show()
-plt.savefig('plot.png')  # Save the plot as an image
-
-
-####################################
-
-# plot(case.results.total_forces.raw["pseudo_step"], case.results.total_forces.raw["CL"])
-# xlabel("pseudo step")
-# ylabel("CL")
-# show()
+# Saving the plot as an image
+plt.savefig('plot.png')
